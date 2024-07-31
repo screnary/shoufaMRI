9c69371a201db68b0d01d63b9fe1d30acb626ac0
screnary <screnary@qq.com>
2024-07-31 18:04:18 +0800

    update getNewDicomData.py

--- getNewDicomData.py
+++ getNewDicomData.py
@@ -44,6 +44,7 @@ def read_dicom_metadata(dataset):
                 print(f"    Item {i}:")
                 for subelem in item:
                     print(f"      {subelem.tag}: {subelem.name} = {subelem.value}")
+            pdb.set_trace()
         else:
             # 对于像素数据，只打印标签和名称，不打印值（因为可能非常大）
             if elem.tag == (0x7fe0, 0x0010):  # Pixel Data
